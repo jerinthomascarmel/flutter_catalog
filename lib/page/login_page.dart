b7da3d01d474651a4819166df6f2dfdbe4220f75
Jerin <jerinthomascarmel@gmail.com>
2023-08-12 21:40:59 +0530

    overflow_resolved

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -6,6 +6,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text(
           "Login Page",
